e196c3f9742d54c089c374aac81cf49b085d4de9
Asset Kadyrzhanov <asset_kad@mail.ru>
2022-09-13 15:50:15 +0500

    added tabs js module

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,39 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach((element) => {
+            element.style.display = 'none';
+        });
+
+        tab.forEach(element => {
+            element.classList.remove(activeClass);
+        });
+    }
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && (
+            target.classList.contains(tabSelector.replace(/\./, '')) ||
+            target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
